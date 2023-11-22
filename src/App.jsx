47482141a5fc4a2b831da3c7438461de5a096043
SuayMack <pry_chu@hotmail.com>
2023-11-22 18:54:15 -0300

    :tada: feat: project started

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Post } from './Post.jsx'
+
+import './styles.css'
+
+export function App() {
+  return (   
+    <>
+      <Post 
+        author="John Doe" 
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+      />
+    </>
+  )
+}
