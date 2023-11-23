99d846ae1e92c2fe8a6129536b7443df5db07429
SuayMack <pry_chu@hotmail.com>
2023-11-23 14:19:52 -0300

    feat: created Post component

---
+++ src/components/Post.jsx
@@ -0,0 +1,49 @@
+import { Comment } from './Comment'
+
+import styles from './Post.module.css'
+
+console.log(Comment)
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="http://github.com/SuayMack.png" />
+          <div className={styles.authorInfo}>
+            <strong>PiscilaCS</strong>
+            <span>Software Engineer</span>
+          </div>
+        </div>
+        <time title="23 de novembro de 2023 às 10:03" dateTime="2023-11-23 08:13:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+
+        <p><a href='#'>👉 jane.design/doctorcare</a></p>
+
+        <p className={styles.hashtags}>
+          <a href='#'>#novoprojeto</a>
+          <a href='#'>#nlw</a>
+          <a href='#'>#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder="Deixe um comentário" />
+        <div>
+          <button type="submit">Publicar</button>
+        </div>
+      </form>
+      <div className={styles.commentList}>
+        <p>Sem comentários</p>
+        <Comment />
+      </div>
+
+    </article>
+  )
+}
