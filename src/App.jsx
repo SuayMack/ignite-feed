6796233233e15305b8230d9377f32b9134fb41be
SuayMack <pry_chu@hotmail.com>
2023-11-23 14:21:14 -0300

    style: adjust css style

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { Post } from './Post.jsx'
+import { Post } from './components/Post.jsx'
 import { Header } from './components/Header.jsx'
-
+import { Sidebar } from './components/Sidebar.jsx'
 
 import styles from './App.module.css'
 import './global.css'
-import { Sidebar } from './components/Sidebar.jsx'
 
 export function App() {
   return (   
@@ -15,14 +14,7 @@ export function App() {
         <Sidebar />
    
         <main>
-          <Post 
-            author="John Doe" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          />
-          <Post 
-            author="John Doe" 
-            content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-          />
+          <Post author="PiscilaCS" content="Fala galeraa 👋"  />
         </main>
       </div>
     </div>
