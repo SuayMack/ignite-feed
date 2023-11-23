26e463cbb3d7b61d7961906734bf13bac6184feb
SuayMack <pry_chu@hotmail.com>
2023-11-23 18:04:20 -0300

    feat: created Comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,25 +2,29 @@ import { ThumbsUp, Trash } from 'phosphor-react'
 import styles from './Comment.module.css'
 
 export function Comment() {
-  <div className={styles.comment}>
-    <img src="http://github.com/SuayMack.png" />
-    <div className={styles.commentBox}>
-      <div className={styles.commentContent}>
-        <header></header>
-        <div className={styles.authorAndTime}>
-          <strong>PiscilaCS</strong>
-          <time title="23 de novembro de 2023 às 10:03" dateTime="2023-11-23 08:13:00">Cerca de 1h atrás</time>
+  return(
+    <div className={styles.comment}>
+      <img src="http://github.com/SuayMack.png" />
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>PiscilaCS</strong>
+              <time title="23 de novembro de 2023 às 10:03" dateTime="2023-11-23 08:13:00">Cerca de 1h atrás</time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Fala galeraa 👋</p>
         </div>
-        <button title="Deletar comentário">
-          <Trash size={24} />
-        </button>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
       </div>
-      <footer>
-        <button>
-          <ThumbsUp />
-          Aplaudir <span>20</span>
-        </button>
-      </footer>
     </div>
-  </div>
+  )
 }
