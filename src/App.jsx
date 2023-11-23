a230c6f792843ec4bb2c86ca511602475584b30e
SuayMack <pry_chu@hotmail.com>
2023-11-23 08:53:21 -0300

    feat: created sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import { Post } from './Post.jsx'
 import { Header } from './components/Header.jsx'
 
+
+import styles from './App.module.css'
 import './global.css'
+import { Sidebar } from './components/Sidebar.jsx'
 
 export function App() {
   return (   
-    <>
+    <div>
       <Header />
-      <Post 
-        author="John Doe" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
-      />
-    </>
+      <div className={styles.wrapper}>
+        
+        <Sidebar />
+   
+        <main>
+          <Post 
+            author="John Doe" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+          <Post 
+            author="John Doe" 
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          />
+        </main>
+      </div>
+    </div>
   )
 }
