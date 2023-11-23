5ebb6270efa2a3013dd3a7f7f4eb364cbd75c706
SuayMack <pry_chu@hotmail.com>
2023-11-22 21:39:07 -0300

    feat: render Header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Post } from './Post.jsx'
+import { Header } from './components/Header.jsx'
 
-import './styles.css'
+import './global.css'
 
 export function App() {
   return (   
     <>
+      <Header />
       <Post 
         author="John Doe" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit."
