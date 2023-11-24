23b81590ca5a78effaab38ce4b1a15fd29698510
SuayMack <pry_chu@hotmail.com>
2023-11-23 22:40:42 -0300

    feat: created avatar component

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,7 +1,8 @@
 import style from './Avatar.module.css'
 
-export function Avatar(props) {
+export function Avatar({ hasBorder = true, src }) {
+
   return (   
-    <img className={style.avatar} src={props.src} />   
+    <img className={hasBorder ? style.avatarHasBorder : style.avatar} src={src} />   
   )
 }
