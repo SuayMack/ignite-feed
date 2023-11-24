e8adc4c4ba8ee8824cb399ed2c7979622985fcdf
SuayMack <pry_chu@hotmail.com>
2023-11-23 23:54:16 -0300

    feat: created iterations

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,43 @@ import { Sidebar } from './components/Sidebar.jsx'
 import styles from './App.module.css'
 import './global.css'
 
+// author: { avatar_url: string; name: string; role: string }
+// publishedAt: Date
+// content: string
+
+// Desafio: -Criar uma API
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/SuayMack.png',
+      name: 'PiscilaCS',
+      role: 'Software Engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-11-23 08:13:00')
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/SuayMack.png',
+      name: 'PiscilaCS',
+      role: 'Software Engineer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉 jane.design/doctorcare' }
+    ],
+    publishedAt: new Date('2023-11-20 12:13:00')
+  }
+]
+
 export function App() {
   return (   
     <div>
@@ -14,7 +51,16 @@ export function App() {
         <Sidebar />
    
         <main>
-          <Post author="PiscilaCS" content="Fala galeraa 👋"  />
+          {posts.map(post => {
+            return (
+              <Post
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
